7d8cecd46139431295ed44213d621d838718a2a7
ryanngab <firdausriyan402@gmail.com>
2024-05-16 13:48:25 +0700

    first commit

--- src/data/constants.js
+++ src/data/constants.js
@@ -398,46 +398,46 @@ export const education = [
   },
 ];
 
-export const projects = [
-  {
-    id: 0,
-    title: "Podstream",
-    date: "Apr 2023 - May 2023",
-    description:
-      "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
-    image:
-      "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-    category: "web app",
-    github: "https://github.com/rishavchanda/Podstream",
-    webapp: "https://podstream.netlify.app/",
-    member: [
-      {
-        name: "Rishav Chanda",
-        img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-        github: "https://github.com/rishavchanda/",
-      },
-      {
-        name: "Upasana Chaudhuri",
-        img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-        github: "https://github.com/upasana0710",
-      },
-    ],
-  },
-  {
-    id: 8,
-    title: "Face Recognition",
-    date: "Jan 2021",
-    description:
-      "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
-    image:
-      "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
-    tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
-    category: "machine learning",
-    github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-    webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
-  },
-];
+// export const projects = [
+//   {
+//     id: 0,
+//     title: "Podstream",
+//     date: "Apr 2023 - May 2023",
+//     description:
+//       "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+//     image:
+//       "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+//     tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+//     category: "web app",
+//     github: "https://github.com/rishavchanda/Podstream",
+//     webapp: "https://podstream.netlify.app/",
+//     member: [
+//       {
+//         name: "Rishav Chanda",
+//         img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+//         linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+//         github: "https://github.com/rishavchanda/",
+//       },
+//       {
+//         name: "Upasana Chaudhuri",
+//         img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+//         linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+//         github: "https://github.com/upasana0710",
+//       },
+//     ],
+//   },
+//   {
+//     id: 8,
+//     title: "Face Recognition",
+//     date: "Jan 2021",
+//     description:
+//       "A Face recognition python app made with OpenCV. It uses face_recognition library to detect faces. It uses the webcam to detect faces. It also has a search bar to search for a particular face.",
+//     image:
+//       "https://dontrepeatyourself.org/media/face-recognition-with-python-dlib-and-deep-learning_cezKZBj.png",
+//     tags: ["Python", "Keras", "TensorFlow", "VGG16", "Pickle", "React"],
+//     category: "machine learning",
+//     github: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+//     webapp: "https://github.com/rishavchanda/Face-Recodnition-AI-with-Python",
+//   },
+// ];
 
